b04ec255cc26016108ba30b0482aeaaaad414fed
Sam Mucyo <90157786+Sam-Mucyo@users.noreply.github.com>
2024-12-07 00:38:12 -0500

    update with correct base api

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = process.env.NODE_ENV === 'production'
-    ? 'https://sam-mucyo.github.io/conf-confy/'
+    ? 'https://conf-confy.onrender.com'
     : 'http://localhost:8000';
 
 
